e6fc63b949e640c0740a48babd44474d5c197948
jeongseolin <tjfls1012@naver.com>
2023-02-14 20:33:46 +0900

    javascript_study_Day1

---
+++ var_list.js
@@ -0,0 +1,10 @@
+var student = {
+    name : "lin",
+    age : 30,
+    skill : ["Javascript","CSS","HTML"],
+    sum : function(num1, num2) {
+        return num1 + num2;
+    }
+}
+//console.log(document.write(student.name));
+console.log(student.sum(10,20));
