9a9c187b5048785474eaf2f06a821dc9e310b34a
jeongseolin <mungseo1012@gmail.com>
2023-03-03 12:10:28 +0900

    elice_0303

--- javascript_study/study_Day3/true.js
+++
@@ -1,8 +0,0 @@
-function solution(s){
-    var change = s.toLowerCase(); // 모두 소문자로 변환
-
-    var check_p = change.split("p").length; // "p"의 개수를 체크
-    var check_y = change.split("y").length; // "y"의 개수를 체크
-    
-    return check_p == check_y ? true : false; // 각 체크된 개수를 비교해서, true/false값 도출
-}
--- javascript_study/study_Day6/break_outer.js
+++
@@ -1,9 +0,0 @@
-// outer라는 식별자가 붙은 레이블 for문
-outer : for (var i = 0; i < 3; i++) {
-    for (var j = 0; j < 3; j++) {
-        // i+J === 3 이면 outer라는 식별자가 붙은 레이블 for문을 탈출한다. 
-        if ( i + j === 3) break outer;
-        console.log(`inner[${i},${j}]`);
-    }
-}
-console.log('Done!');
