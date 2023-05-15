ddeabedaa3e3083d6be54c76af04b1dbe8c7e8c4
mungseo1012 <tjfls1012@naver.com>
2023-05-15 20:44:12 +0900

    elice_0515_1

---
+++ daily_study/elice_camp_v/elice_0515/1_LoginForm.jsx
@@ -0,0 +1,51 @@
+import React, { useRef } from "react";
+import { useHistory } from "react-router-dom";
+
+export default function LoginForm() {
+  const history = useHistory();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    
+    // DetailPage로 이동하는 코드를 작성하세요.
+    history.push(`/detail?email=${email}&password=${password}`);
+  };
+
+  return (
+    <div>
+      <form>
+        <fieldset>
+          <label htmlFor="email">Email</label>
+          <input
+            placeholder="Enter email."
+            required
+            ref={emailRef}
+            id="email"
+            type="email"
+            name="email"
+            autoComplete="off"
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="password">Password</label>
+          <input
+            required
+            ref={passwordRef}
+            id="password"
+            type="password"
+            name="password"
+            placeholder="Enter password."
+          />
+        </fieldset>
+        <button type="submit" onClick={submitForm}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
